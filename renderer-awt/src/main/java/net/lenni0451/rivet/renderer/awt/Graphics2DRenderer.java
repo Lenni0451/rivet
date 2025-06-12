e4d04bcfd4c140b12b73c66ee89838130347dc01
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-06-12 20:15:11 +0200

    Make awt test fully functional

--- renderer-awt/src/main/java/net/lenni0451/rivet/renderer/awt/Graphics2DRenderer.java
+++ renderer-awt/src/main/java/net/lenni0451/rivet/renderer/awt/Graphics2DRenderer.java
@@ -7,20 +7,26 @@ import org.joml.Matrix4f;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
+import java.util.function.Supplier;
 
 public class Graphics2DRenderer implements Renderer {
 
-    private final Graphics2D g2d;
+    private final Supplier<Graphics2D> g2dSupplier;
 
     public Graphics2DRenderer(final Graphics2D g2d) {
-        this.g2d = g2d;
+        this.g2dSupplier = () -> g2d;
+    }
+
+    public Graphics2DRenderer(final Supplier<Graphics2D> g2d) {
+        this.g2dSupplier = g2d;
     }
 
     @Override
     public void filledRectangle(Matrix4f positionMatrix, float x, float y, float width, float height, Color color) {
-        this.applyColor(color);
-        this.applyTransform(positionMatrix);
-        this.g2d.fill(new Rectangle2D.Float(x, y, width, height));
+        Graphics2D g2d = this.g2dSupplier.get();
+        this.applyColor(g2d, color);
+        this.applyTransform(g2d, positionMatrix);
+        g2d.fill(new Rectangle2D.Float(x, y, width, height));
     }
 
     @Override
@@ -31,12 +37,12 @@ public class Graphics2DRenderer implements Renderer {
     public void endBatch() {
     }
 
-    private void applyColor(final Color color) {
-        this.g2d.setColor(new java.awt.Color(color.toARGB(), true));
+    private void applyColor(final Graphics2D g2d, final Color color) {
+        g2d.setColor(new java.awt.Color(color.toARGB(), true));
     }
 
-    private void applyTransform(final Matrix4f positionMatrix) {
-        this.g2d.setTransform(new AffineTransform(
+    private void applyTransform(final Graphics2D g2d, final Matrix4f positionMatrix) {
+        g2d.setTransform(new AffineTransform(
                 positionMatrix.m00(), positionMatrix.m10(),
                 positionMatrix.m10(), positionMatrix.m11(),
                 positionMatrix.m30(), positionMatrix.m31()
--- renderer-awt/src/test/java/Test.java
+++ renderer-awt/src/test/java/Test.java
@@ -4,13 +4,24 @@ import net.lenni0451.rivet.container.impl.AbsoluteContainer;
 import net.lenni0451.rivet.renderer.awt.Graphics2DRenderer;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-public class Test {
+public class Test extends Canvas {
+
+    static {
+        System.setProperty("sun.java2d.uiScale", "1.0");
+        System.setProperty("glass.win.uiScale", "100%");
+        System.setProperty("prism.allowhidpi", "false");
+    }
 
     public static void main(String[] args) throws Throwable {
+        new Test().run();
         BufferedImage image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = image.createGraphics();
         AbsoluteContainer rootContainer = new AbsoluteContainer();
@@ -20,6 +31,77 @@ public class Test {
         rivet.render();
 
         ImageIO.write(image, "png", new File("test.png"));
+    }
+
+
+    private JFrame frame;
+    private Rivet rivet;
+    private Graphics2D g2d;
+
+    public Test() {
+        this.initFrame();
+        this.initUI();
+        this.initListeners();
+    }
+
+    private void initFrame() {
+        this.frame = new JFrame("Rivet Test");
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(500, 500);
+        this.frame.setLocationRelativeTo(null);
+        JPanel root = new JPanel(new BorderLayout());
+        this.frame.setContentPane(root);
+        root.add(this, BorderLayout.CENTER);
+        this.frame.setVisible(true);
+    }
+
+    private void initUI() {
+        AbsoluteContainer rootContainer = new AbsoluteContainer();
+        Button button = new Button("Testing", mouseButton -> System.out.println("CLICKED! Button: " + mouseButton));
+        rootContainer.add(button, 50, 50);
+        this.rivet = new Rivet(new Graphics2DRenderer(() -> this.g2d), rootContainer, this.frame.getWidth(), this.frame.getHeight());
+    }
+
+    private void initListeners() {
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                Test.this.rivet.onMouseDown(e.getX(), e.getY(), e.getButton());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                Test.this.rivet.onMouseUp(e.getX(), e.getY(), e.getButton());
+            }
+        });
+        this.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                Test.this.rivet.onMouseMove(e.getX(), e.getY());
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                Test.this.rivet.onMouseMove(e.getX(), e.getY());
+            }
+        });
+    }
+
+    private void run() {
+        this.createBufferStrategy(2);
+        BufferStrategy bufferStrategy = this.getBufferStrategy();
+        while (!Thread.currentThread().isInterrupted()) {
+            do {
+                do {
+                    final Graphics graphics = bufferStrategy.getDrawGraphics();
+                    if (graphics == null) continue;
+                    this.g2d = (Graphics2D) graphics;
+                    this.rivet.render();
+                    graphics.dispose();
+                } while (bufferStrategy.contentsRestored());
+                bufferStrategy.show();
+            } while (bufferStrategy.contentsLost());
+        }
     }
 
 }
