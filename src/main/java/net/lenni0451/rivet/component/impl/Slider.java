e58828e5fa8a6b52ba8041d17d1feb9fa439ac40
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-04-09 00:11:18 +0200

    Improved code

--- src/main/java/net/lenni0451/rivet/component/impl/Slider.java
+++ src/main/java/net/lenni0451/rivet/component/impl/Slider.java
@@ -20,6 +20,8 @@ import net.lenni0451.rivet.theme.Theme;
 import net.lenni0451.rivet.theme.ThemeOption;
 
 import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
 
 @Accessors(chain = true, fluent = true)
 public class Slider extends Component implements MouseListener, Renderable {
@@ -27,10 +29,8 @@ public class Slider extends Component implements MouseListener, Renderable {
     private static final int TICK_OFFSET = 2;
 
     @Getter
-    @Setter
     private double min;
     @Getter
-    @Setter
     private double max;
     @Getter
     @Setter
@@ -39,10 +39,10 @@ public class Slider extends Component implements MouseListener, Renderable {
     @Setter
     private double value;
     @Getter
-    @Setter
     @Nullable
     private Ticks ticks;
     private boolean dragged;
+    private final Map<Double, ShapedText> tickLabels = new HashMap<>();
 
     @Getter
     private final ThemeOption<Color> barColor;
@@ -85,6 +85,24 @@ public class Slider extends Component implements MouseListener, Renderable {
         this.thumbShape = new ThemeOption<>(rivet, Theme.SLIDER_THUMB_SHAPE, () -> ThumbShape.CIRCLE);
     }
 
+    public Slider min(final double min) {
+        this.min = min;
+        this.tickLabels.clear();
+        return this;
+    }
+
+    public Slider max(final double max) {
+        this.max = max;
+        this.tickLabels.clear();
+        return this;
+    }
+
+    public Slider ticks(@Nullable final Ticks ticks) {
+        this.ticks = ticks;
+        this.tickLabels.clear();
+        return this;
+    }
+
     @Override
     public void onMouseDown(final MouseButtonEvent event, final Size size) {
         if (event.button().equals(MouseButton.LEFT)) {
@@ -121,60 +139,70 @@ public class Slider extends Component implements MouseListener, Renderable {
     public void render(final Renderer renderer, final Size size) {
         float thumbRadius = this.thumbRadius.value();
         float barHeight = this.barHeight.value();
-        float sliderCenter = size.height() / 2F;
-        if (this.ticks != null) {
-            sliderCenter = thumbRadius;
-        }
+        float sliderCenter = this.ticks != null ? thumbRadius : size.height() / 2F;
+        float barWidth = this.barWidth(size);
+        double progress = (this.value - this.min) / (this.max - this.min);
+        float thumbX = (float) (thumbRadius + barWidth * progress);
 
+        this.renderBar(renderer, size, sliderCenter, barHeight, thumbRadius);
+        this.renderThumb(renderer, sliderCenter, thumbRadius, thumbX);
+        if (this.ticks != null) this.renderTicks(renderer, sliderCenter, barHeight, thumbRadius, barWidth);
+    }
+
+    private void renderBar(final Renderer renderer, final Size size, final float sliderCenter, final float barHeight, final float thumbRadius) {
         if (this.thumbEncased.value()) {
             renderer.fillOptimizedRoundedRect(0, sliderCenter - barHeight / 2F, size.width(), barHeight, this.barCornerRadius.value(), this.barColor.value());
         } else {
             renderer.fillOptimizedRoundedRect(thumbRadius / 2, sliderCenter - barHeight / 2F, size.width() - thumbRadius, barHeight, this.barCornerRadius.value(), this.barColor.value());
         }
-        float barWidth = this.barWidth(size);
-        double progress = (this.value - this.min) / (this.max - this.min);
-        float thumbX = (float) (thumbRadius + barWidth * progress);
+    }
+
+    private void renderThumb(final Renderer renderer, final float sliderCenter, final float thumbRadius, final float thumbX) {
+        Color color = this.thumbColor.value();
+        int cornerRadius = this.thumbCornerRadius.value();
         switch (this.thumbShape.value()) {
-            case CIRCLE, SQUARE -> renderer.fillOptimizedRoundedRect(thumbX - thumbRadius, sliderCenter - thumbRadius, thumbRadius * 2, thumbRadius * 2, this.thumbCornerRadius.value(), this.thumbColor.value());
-            case RECTANGLE -> renderer.fillOptimizedRoundedRect(thumbX - thumbRadius / 2F, sliderCenter - thumbRadius, thumbRadius, thumbRadius * 2, this.thumbCornerRadius.value(), this.thumbColor.value());
+            case CIRCLE, SQUARE -> renderer.fillOptimizedRoundedRect(thumbX - thumbRadius, sliderCenter - thumbRadius, thumbRadius * 2, thumbRadius * 2, cornerRadius, color);
+            case RECTANGLE -> renderer.fillOptimizedRoundedRect(thumbX - thumbRadius / 2F, sliderCenter - thumbRadius, thumbRadius, thumbRadius * 2, cornerRadius, color);
             case PIN -> {
-                renderer.fillOptimizedRoundedRect(thumbX - thumbRadius, sliderCenter - thumbRadius, thumbRadius * 2, thumbRadius, this.thumbCornerRadius.value(), this.thumbColor.value());
-                renderer.fillTriangle(thumbX - thumbRadius, sliderCenter, thumbX, sliderCenter + thumbRadius, thumbX + thumbRadius, sliderCenter, this.thumbColor.value());
+                renderer.fillOptimizedRoundedRect(thumbX - thumbRadius, sliderCenter - thumbRadius, thumbRadius * 2, thumbRadius, cornerRadius, color);
+                renderer.fillTriangle(thumbX - thumbRadius, sliderCenter, thumbX, sliderCenter + thumbRadius, thumbX + thumbRadius, sliderCenter, color);
             }
         }
+    }
 
-        if (this.ticks != null) {
-            float tickStartY = sliderCenter + thumbRadius + TICK_OFFSET;
-            float majorTickLength = barHeight;
-            float minorTickLength = barHeight / 2F;
+    private void renderTicks(final Renderer renderer, final float sliderCenter, final float barHeight, final float thumbRadius, final float barWidth) {
+        float tickStartY = sliderCenter + thumbRadius + TICK_OFFSET;
+        float majorTickLength = barHeight;
+        float minorTickLength = barHeight / 2F;
+        Color color = this.tickColor.value();
 
-            if (this.ticks.majorTickSpacing > 0) {
-                for (double tick = 0; ; tick += this.ticks.majorTickSpacing) {
-                    boolean lastTick = false;
-                    if (tick >= this.max - this.min) {
-                        tick = this.max - this.min;
-                        lastTick = true;
-                    }
-                    float tickX = (float) (thumbRadius + barWidth * (tick / (this.max - this.min)));
-                    renderer.fillRect(tickX - 1, tickStartY, TICK_OFFSET, majorTickLength, this.tickColor.value());
-                    String label = this.ticks.labelProvider.getLabel(this.min + tick);
-                    ShapedText text = this.rivet.getBackend().shapeText(label);
-                    renderer.push();
-                    renderer.translate(tickX, tickStartY + majorTickLength + 2);
-                    renderer.scale(0.5F);
-                    renderer.renderText(text, 0, 0, TextOrigin.Horizontal.VISUAL_CENTER, TextOrigin.Vertical.LOGICAL_TOP);
-                    renderer.pop();
-                    if (lastTick) break;
+        if (this.ticks.majorTickSpacing > 0) {
+            for (double tick = 0; ; tick += this.ticks.majorTickSpacing) {
+                boolean lastTick = false;
+                if (tick >= this.max - this.min) {
+                    tick = this.max - this.min;
+                    lastTick = true;
                 }
+                float tickX = (float) (thumbRadius + barWidth * (tick / (this.max - this.min)));
+                renderer.fillRect(tickX - 1, tickStartY, TICK_OFFSET, majorTickLength, color);
+
+                double tickValue = this.min + tick;
+                ShapedText text = this.tickLabels.computeIfAbsent(tickValue, v -> this.rivet.getBackend().shapeText(this.ticks.labelProvider.getLabel(v)));
+                renderer.push();
+                renderer.translate(tickX, tickStartY + majorTickLength + 2);
+                renderer.scale(0.5F);
+                renderer.renderText(text, 0, 0, TextOrigin.Horizontal.VISUAL_CENTER, TextOrigin.Vertical.LOGICAL_TOP);
+                renderer.pop();
+                if (lastTick) break;
             }
-            if (this.ticks.minorTickSpacing > 0) {
-                for (double tick = 0; tick < this.max - this.min; tick += this.ticks.minorTickSpacing) {
-                    if (this.ticks.majorTickSpacing > 0 && Math.abs(tick % this.ticks.majorTickSpacing) < 1e-6) {
-                        continue; //Skip if it's a major tick
-                    }
-                    float tickX = (float) (thumbRadius + barWidth * (tick / (this.max - this.min)));
-                    renderer.fillRect(tickX, tickStartY, 1, minorTickLength, this.tickColor.value());
+        }
+        if (this.ticks.minorTickSpacing > 0) {
+            for (double tick = 0; tick < this.max - this.min; tick += this.ticks.minorTickSpacing) {
+                if (this.ticks.majorTickSpacing > 0 && Math.abs(tick % this.ticks.majorTickSpacing) < 1e-6) {
+                    continue;
                 }
+                float tickX = (float) (thumbRadius + barWidth * (tick / (this.max - this.min)));
+                renderer.fillRect(tickX, tickStartY, 1, minorTickLength, color);
             }
         }
     }
