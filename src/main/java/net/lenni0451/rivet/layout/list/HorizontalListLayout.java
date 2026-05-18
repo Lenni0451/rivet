41678d1a1627c3772ec280b32ddb70a459a0e806
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-05-18 22:24:09 +0200

    Added list layouts

---
+++ src/main/java/net/lenni0451/rivet/layout/list/HorizontalListLayout.java
@@ -0,0 +1,46 @@
+package net.lenni0451.rivet.layout.list;
+
+import lombok.RequiredArgsConstructor;
+import net.lenni0451.rivet.component.Component;
+import net.lenni0451.rivet.layout.Layout;
+import net.lenni0451.rivet.math.Rectangle;
+import net.lenni0451.rivet.math.Size;
+
+import java.util.Collection;
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class HorizontalListLayout implements Layout {
+
+    private final int gap;
+
+    @Override
+    public Size computeIdealSize(final Size constraints, final Collection<Component> components) {
+        float width = 0;
+        float height = 0;
+        for (Component component : components) {
+            width += this.widthOf(component);
+            height = Math.max(height, this.heightOf(component));
+        }
+        if (!components.isEmpty()) {
+            width += this.gap * (components.size() - 1);
+        }
+        return new Size(width, height);
+    }
+
+    @Override
+    public Map<Component, Rectangle> layoutComponents(final Size containerSize, final Collection<Component> components) {
+        Map<Component, Rectangle> layout = new IdentityHashMap<>();
+        float x = 0;
+        for (Component component : components) {
+            float width = this.widthOf(component);
+            float height = this.heightOf(component);
+            if (x > 0) x += this.gap;
+            layout.put(component, new Rectangle(x, 0, width, height));
+            x += width;
+        }
+        return layout;
+    }
+
+}
---
+++ src/main/java/net/lenni0451/rivet/layout/list/VerticalListLayout.java
@@ -0,0 +1,46 @@
+package net.lenni0451.rivet.layout.list;
+
+import lombok.RequiredArgsConstructor;
+import net.lenni0451.rivet.component.Component;
+import net.lenni0451.rivet.layout.Layout;
+import net.lenni0451.rivet.math.Rectangle;
+import net.lenni0451.rivet.math.Size;
+
+import java.util.Collection;
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class VerticalListLayout implements Layout {
+
+    private final int gap;
+
+    @Override
+    public Size computeIdealSize(final Size constraints, final Collection<Component> components) {
+        float width = 0;
+        float height = 0;
+        for (Component component : components) {
+            width = Math.max(width, this.widthOf(component));
+            height += this.heightOf(component);
+        }
+        if (!components.isEmpty()) {
+            height += this.gap * (components.size() - 1);
+        }
+        return new Size(width, height);
+    }
+
+    @Override
+    public Map<Component, Rectangle> layoutComponents(final Size containerSize, final Collection<Component> components) {
+        Map<Component, Rectangle> layout = new IdentityHashMap<>();
+        float y = 0;
+        for (Component component : components) {
+            float width = this.widthOf(component);
+            float height = this.heightOf(component);
+            if (y != 0) y += this.gap;
+            layout.put(component, new Rectangle(0, y, width, height));
+            y += this.heightOf(component);
+        }
+        return layout;
+    }
+
+}
