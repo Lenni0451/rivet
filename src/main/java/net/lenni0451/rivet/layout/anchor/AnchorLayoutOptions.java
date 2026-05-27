1dcfa3ccf72573b49fc95d4b45f56d27b1d8588f
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-05-27 10:59:45 +0200

    Added some useful methods

--- src/main/java/net/lenni0451/rivet/layout/anchor/AnchorLayoutOptions.java
+++ src/main/java/net/lenni0451/rivet/layout/anchor/AnchorLayoutOptions.java
@@ -14,4 +14,21 @@ public record AnchorLayoutOptions(
 
     public static final AnchorLayoutOptions EMPTY = new AnchorLayoutOptions(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
+    public static AnchorLayoutOptions point(final float x, final float y) {
+        return new AnchorLayoutOptions(x, x, y, y, 0, 0, 0, 0, 0, 0);
+    }
+
+
+    public AnchorLayoutOptions(final float anchorMinX, final float anchorMaxX, final float anchorMinY, final float anchorMaxY) {
+        this(anchorMinX, anchorMaxX, anchorMinY, anchorMaxY, 0, 0, 0, 0, 0, 0);
+    }
+
+    public AnchorLayoutOptions from(final float x, final float y) {
+        return this.withAnchorMinX(x).withAnchorMinY(y);
+    }
+
+    public AnchorLayoutOptions to(final float x, final float y) {
+        return this.withAnchorMaxX(x).withAnchorMaxY(y);
+    }
+
 }
