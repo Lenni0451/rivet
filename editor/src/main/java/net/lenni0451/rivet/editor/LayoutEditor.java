688bb8ca9a913182b0ca8cfb84e5859bfb89aea2
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-08-03 08:43:20 +0200

    Started implementing component field

--- editor/src/main/java/net/lenni0451/rivet/editor/LayoutEditor.java
+++ editor/src/main/java/net/lenni0451/rivet/editor/LayoutEditor.java
@@ -1,12 +1,18 @@
 package net.lenni0451.rivet.editor;
 
+import net.lenni0451.rivet.component.container.Button;
 import net.lenni0451.rivet.component.container.Container;
+import net.lenni0451.rivet.component.impl.Label;
+import net.lenni0451.rivet.editor.editor.ComponentField;
 import net.lenni0451.rivet.layout.border.BorderLayout;
+import net.lenni0451.rivet.layout.border.BorderPosition;
 
 public class LayoutEditor extends Container {
 
     public LayoutEditor() {
         super(BorderLayout.DEFAULT);
+
+        this.addChild(new ComponentField().addChild(new Button("Test 1")).addChild(new Label("Test 2")).layoutOptions(BorderPosition.CENTER));
     }
 
 }
