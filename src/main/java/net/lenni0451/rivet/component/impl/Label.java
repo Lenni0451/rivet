1a933b3c8cada08b4cca90fa1f29b0bcaf1579d1
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-08-09 23:12:08 +0200

    Added wrap behavior to label

--- src/main/java/net/lenni0451/rivet/component/impl/Label.java
+++ src/main/java/net/lenni0451/rivet/component/impl/Label.java
@@ -7,11 +7,16 @@ import net.lenni0451.commons.color.Color;
 import net.lenni0451.rivet.backend.render.Renderer;
 import net.lenni0451.rivet.backend.text.Font;
 import net.lenni0451.rivet.backend.text.ShapedText;
+import net.lenni0451.rivet.backend.text.ShapedTextBlock;
 import net.lenni0451.rivet.component.Component;
 import net.lenni0451.rivet.input.mouse.MouseButtonEvent;
 import net.lenni0451.rivet.input.mouse.MouseMoveEvent;
 import net.lenni0451.rivet.math.Size;
+import net.lenni0451.rivet.text.TextWrapper;
+import net.lenni0451.rivet.text.model.TextFormat;
+import net.lenni0451.rivet.text.model.TextLine;
 import net.lenni0451.rivet.text.model.TextOrigin;
+import net.lenni0451.rivet.text.model.TextSection;
 import net.lenni0451.rivet.theme.Theme;
 import net.lenni0451.rivet.theme.ThemeOption;
 
@@ -26,6 +31,7 @@ public class Label extends Component {
     @Getter
     private String text;
     private ShapedText shapedText;
+    private ShapedTextBlock shapedTextBlock;
     private boolean reshape;
     @Getter
     private final ThemeOption<Color> textColor = new ThemeOption<>(this, Theme.General.TEXT_COLOR);
@@ -40,6 +46,9 @@ public class Label extends Component {
     @Setter
     private TextOrigin.Vertical verticalOrigin = TextOrigin.Vertical.LOGICAL_CENTER;
     @Getter
+    @Setter
+    private ShapedTextBlock.LineAlignment lineAlignment = ShapedTextBlock.LineAlignment.LEFT;
+    @Getter
     private float scale = 1F;
 
     public Label(final String text) {
@@ -47,6 +56,12 @@ public class Label extends Component {
 
         this.textColor.changeListener().add(c -> this.reshape = true);
         this.disabledTextColor.changeListener().add(c -> this.reshape = true);
+        this.overflowBehavior.changeListener().add(o -> {
+            this.reshape = true;
+            if (this.parent() != null) {
+                this.parent().requestLayoutRecalculation();
+            }
+        });
     }
 
     public final Label font(@Nullable final Font font) {
@@ -89,6 +104,12 @@ public class Label extends Component {
         }
     }
 
+    private TextLine createTextLine() {
+        Color textColor = this.disabled() ? this.disabledTextColor.value() : this.textColor.value();
+        TextFormat format = TextFormat.DEFAULT.withColor(textColor);
+        return new TextLine(new TextSection(this.text, format));
+    }
+
     protected final Font usedFont() {
         return this.font != null ? this.font : this.rivet().backend().font();
     }
@@ -125,35 +146,59 @@ public class Label extends Component {
 
     @Override
     public void render(final Renderer renderer, final Size size) {
-        this.shapeText();
-
-        float scale;
-        if (this.overflowBehavior.value().equals(OverflowBehavior.SCALE)) {
-            float widthRatio = size.width() / (this.shapedText.visualBounds().width() * this.scale);
-            float heightRatio = size.height() / (this.shapedText.logicalBounds().height() * this.scale);
-            float ratio = Math.min(widthRatio, heightRatio);
-            scale = ratio > 1 ? this.scale : ratio;
+        if (this.overflowBehavior.value().equals(OverflowBehavior.WRAP)) {
+            if (this.shapedTextBlock == null) {
+                this.computeLayout(size);
+            }
+            float x = this.horizontalOrigin.position(size.width() / this.scale);
+            float y = this.verticalOrigin.position(size.height() / this.scale);
+            renderer.scale(this.scale, () -> renderer.text(this.shapedTextBlock, x, y, this.horizontalOrigin, this.verticalOrigin, this.lineAlignment));
         } else {
-            scale = this.scale;
-        }
+            this.shapeText();
 
-        float x = this.horizontalOrigin.position(size.width() / scale);
-        float y = this.verticalOrigin.position(size.height() / scale);
-        renderer.scale(scale, () -> renderer.text(this.shapedText, x, y, this.horizontalOrigin, this.verticalOrigin));
+            float scale;
+            if (this.overflowBehavior.value().equals(OverflowBehavior.SCALE)) {
+                float widthRatio = size.width() / (this.shapedText.visualBounds().width() * this.scale);
+                float heightRatio = size.height() / (this.shapedText.logicalBounds().height() * this.scale);
+                float ratio = Math.min(widthRatio, heightRatio);
+                scale = ratio > 1 ? this.scale : ratio;
+            } else {
+                scale = this.scale;
+            }
+
+            float x = this.horizontalOrigin.position(size.width() / scale);
+            float y = this.verticalOrigin.position(size.height() / scale);
+            renderer.scale(scale, () -> renderer.text(this.shapedText, x, y, this.horizontalOrigin, this.verticalOrigin));
+        }
     }
 
     @Override
     public Size computeIdealSize(final Size constraints) {
-        this.shapeText();
-        return new Size(
-                this.shapedText.visualBounds().width() * this.scale,
-                this.shapedText.logicalBounds().height() * this.scale
-        );
+        if (this.overflowBehavior.value().equals(OverflowBehavior.WRAP)) {
+            ShapedTextBlock block = TextWrapper.wrapLine(this.usedFont(), this.createTextLine(), constraints.width() / this.scale);
+            return new Size(
+                    block.visualBounds().width() * this.scale,
+                    block.logicalBounds().height() * this.scale
+            );
+        } else {
+            this.shapeText();
+            return new Size(
+                    this.shapedText.visualBounds().width() * this.scale,
+                    this.shapedText.logicalBounds().height() * this.scale
+            );
+        }
+    }
+
+    @Override
+    public void computeLayout(final Size size) {
+        if (this.overflowBehavior.value().equals(OverflowBehavior.WRAP)) {
+            this.shapedTextBlock = TextWrapper.wrapLine(this.usedFont(), this.createTextLine(), size.width() / this.scale);
+        }
     }
 
 
     public enum OverflowBehavior {
-        CLIP, SCALE
+        CLIP, SCALE, WRAP
     }
 
 }
